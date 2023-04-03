70cd8940f1786916ec7ac85a029402cf935b9644
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2023-04-03 22:24:18 +0200

    Finishing up home screen

--- lib/main.dart
+++ lib/main.dart
@@ -79,7 +79,7 @@ class _HomeWidgetState extends State<HomeWidget> {
               padding: const EdgeInsets.all(16),
               decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(8),
-                color: Color.fromARGB(255, 255, 179, 104),
+                color: const Color.fromARGB(255, 255, 179, 104),
               ),
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -87,12 +87,11 @@ class _HomeWidgetState extends State<HomeWidget> {
                     const Text(
                       "Be a real examiner",
                       style: TextStyle(
-                        fontSize: 28,
-                        fontFamily: "DMSans",
-                        fontWeight: FontWeight.w900,
-                        letterSpacing: 1,
-                        color: Colors.black
-                      ),
+                          fontSize: 28,
+                          fontFamily: "DMSans",
+                          fontWeight: FontWeight.w900,
+                          letterSpacing: 1,
+                          color: Colors.black),
                     ),
                     const SizedBox(
                       height: 12,
@@ -124,11 +123,235 @@ class _HomeWidgetState extends State<HomeWidget> {
                     ),
                   ]),
             ),
+            const SizedBox(
+              height: 16,
+            ),
             const Text(
               "Latest courses",
               style: TextStyle(fontSize: 22, fontFamily: "DMSans"),
             ),
-            ListView()
+            Container(
+              height: 211,
+              margin: const EdgeInsetsDirectional.symmetric(vertical: 8),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(2, 8, 8, 8),
+                    child: InkWell(
+                      child: Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.white,
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromRGBO(133, 133, 133, 0.307),
+                                offset: Offset(
+                                  0.5,
+                                  0.5,
+                                ),
+                                blurRadius: 2.0,
+                                spreadRadius: 0.8,
+                              ),
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    color: const Color.fromARGB(255, 245, 245, 245)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SvgPicture.asset(
+                                    'assets/icons/lock-1.svg',
+                                    width: 36,
+                                    height: 36,
+                                    colorFilter: const ColorFilter.mode(
+                                        Color.fromARGB(255, 66, 66, 66), BlendMode.srcIn),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                "Cybersecurity",
+                                style: TextStyle(
+                                    fontFamily: "DMSans", fontSize: 24),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 32, vertical: 12),
+                                    backgroundColor: const Color.fromARGB(255, 245, 245, 245),
+                                    shape: const StadiumBorder()),
+                                // onPressed: () => print('Hello'),
+                                onPressed: null,
+                                child: const Text(
+                                  'Go to courses',
+                                  style: TextStyle(
+                                      fontFamily: "DMSans",
+                                      color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                    child: InkWell(
+                      child: Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.white,
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromRGBO(133, 133, 133, 0.307),
+                                offset: Offset(
+                                  0.5,
+                                  0.5,
+                                ),
+                                blurRadius: 2.0,
+                                spreadRadius: 0.8,
+                              ),
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    color: const Color.fromARGB(255, 245, 245, 245)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SvgPicture.asset(
+                                    'assets/icons/banknote-1.svg',
+                                    width: 36,
+                                    height: 36,
+                                    colorFilter: const ColorFilter.mode(
+                                        Color.fromARGB(255, 66, 66, 66), BlendMode.srcIn),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                "Trigonometry",
+                                style: TextStyle(
+                                    fontFamily: "DMSans", fontSize: 24),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 32, vertical: 12),
+                                    backgroundColor: const Color.fromARGB(255, 245, 245, 245),
+                                    shape: const StadiumBorder()),
+                                // onPressed: () => print('Hello'),
+                                onPressed: null,
+                                child: const Text(
+                                  'Go to courses',
+                                  style: TextStyle(
+                                      fontFamily: "DMSans",
+                                      color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 8),
+                    child: InkWell(
+                      child: Container(
+                        width: 200,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: Colors.white,
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Color.fromRGBO(133, 133, 133, 0.307),
+                                offset: Offset(
+                                  0.5,
+                                  0.5,
+                                ),
+                                blurRadius: 2.0,
+                                spreadRadius: 0.8,
+                              ),
+                            ]),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(100),
+                                    color: const Color.fromARGB(255, 245, 245, 245)),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: SvgPicture.asset(
+                                    'assets/icons/micro-1.svg',
+                                    width: 36,
+                                    height: 36,
+                                    colorFilter: const ColorFilter.mode(
+                                        Color.fromARGB(255, 66, 66, 66), BlendMode.srcIn),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              const Text(
+                                "Listening",
+                                style: TextStyle(
+                                    fontFamily: "DMSans", fontSize: 24),
+                              ),
+                              const SizedBox(
+                                height: 16,
+                              ),
+                              TextButton(
+                                style: TextButton.styleFrom(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 32, vertical: 12),
+                                    backgroundColor: const Color.fromARGB(255, 245, 245, 245),
+                                    shape: const StadiumBorder()),
+                                // onPressed: () => print('Hello'),
+                                onPressed: null,
+                                child: const Text(
+                                  'Go to courses',
+                                  style: TextStyle(
+                                      fontFamily: "DMSans",
+                                      color: Colors.black),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
           ],
         ),
       ),
