584034814277925927acb9b504d4037e8f7dcb84
Nour Eldin Samir <nourelddine.sam302@gmail.com>
2023-05-01 01:53:34 +0300

    Finishing up quizzes screen

--- lib/models/quiz.dart
+++ lib/models/quiz.dart
@@ -1,11 +1,27 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Quiz {
-  String quid = '';
-  final String title;
-  final Timestamp createdAt;
-  final String duration;
-  final String cuid;
+  String quid;
+  String title;
+  Timestamp createdAt;
+  String duration;
+  DocumentReference? cuid;
 
-  Quiz(this.title, this.createdAt, this.duration, this.cuid);
+  Quiz(
+    this.quid,
+    this.title,
+    this.createdAt,
+    this.duration,
+  );
+
+  factory Quiz.fromDocument(DocumentSnapshot doc, String courseId) {
+    final data = doc.data() as Map<String, dynamic>;
+
+    return Quiz(
+      doc.id,
+      data['title'],
+      data['createdAt'],
+      data['duration'],
+    );
+  }
 }
--- lib/screens/course/single_course.dart
+++ lib/screens/course/single_course.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:quizzed/models/course.dart';
+import 'package:quizzed/models/quiz.dart';
 import 'package:quizzed/providers/course_provider.dart';
-// import 'package:quizzed/providers/quiz_provider.dart';
+import 'package:quizzed/providers/quiz_provider.dart';
 
 class SingleCourseScreen extends StatelessWidget {
   static const routeName = '/course';
+
   const SingleCourseScreen({super.key});
 
   String timestampToDateTime(Timestamp timestamp) {
@@ -20,98 +22,89 @@ class SingleCourseScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final courseId = ModalRoute.of(context)!.settings.arguments as String;
     final courseProvider = Provider.of<CourseProvider>(context);
-    // final quizProvider = Provider.of<QuizProvider>(context);
+    final quizProvider = Provider.of<QuizProvider>(context);
 
-    return StreamBuilder<List<Course>>(
-      stream: courseProvider.coursesStream,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
+    final Course course = courseProvider.getSingleCourse(courseId);
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(course.name),
+      ),
+      body: Column(
+        children: [
+          Image.network(course.imageUrl),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  course.name,
+                  style: Theme.of(context).textTheme.displayMedium,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  'Created at: ${timestampToDateTime(course.createdAt)}',
+                  style: Theme.of(context).textTheme.bodyLarge,
+                ),
+              ],
             ),
-          );
-        }
-
-        final courses = snapshot.data ?? [];
-        courseProvider.setCourses(courses);
-
-        final Course course = courseProvider.getSingleCourse(courseId);
-        // var availableQuizzes = quizProvider.getQuizzes(courseId);
-
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(course.name),
           ),
-          body: Column(
-            children: [
-              Image.network(course.imageUrl),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      course.name,
-                      style: Theme.of(context).textTheme.displayMedium,
-                    ),
-                    const SizedBox(
-                      height: 8,
-                    ),
-                    Text(
-                      'Created at: ${timestampToDateTime(course.createdAt)}',
-                      style: Theme.of(context).textTheme.bodyLarge,
-                    ),
-                    const SizedBox(
-                      height: 24,
-                    ),
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: OutlinedButton(
-                            onPressed: null,
-                            child: Text(
-                              'View quizzes',
-                              style: Theme.of(context).textTheme.labelMedium,
-                            ),
+          Expanded(
+            child: StreamBuilder<List<Quiz>>(
+              stream: quizProvider.getAllQuizzes(courseId),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text('Error: ${snapshot.error}'),
+                  );
+                }
+
+                final quizzes = snapshot.data ?? [];
+                quizProvider.setQuizzes(quizzes);
+
+                return ListView.separated(
+                  itemCount: quizzes.length,
+                  separatorBuilder: (BuildContext context, int index) =>
+                      const SizedBox(),
+                  itemBuilder: (BuildContext context, int index) {
+                    final quiz = quizzes[index];
+                    return Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: InkWell(
+                        onTap: null,
+                        child: Container(
+                          padding: const EdgeInsets.all(16.0),
+                          color: Colors.white,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                quiz.title,
+                                style: Theme.of(context).textTheme.displaySmall,
+                              ),
+                              Text(timestampToDateTime(quiz.createdAt)),
+                              Text('${quiz.duration} min(s)'),
+                            ],
                           ),
                         ),
-                        const Expanded(
-                          flex: 1,
-                          child: SizedBox(),
-                        ),
-                      ],
-                    )
-                    // StreamBuilder(
-                    //     stream: availableQuizzes,
-                    //     builder: (context, snapshot) {
-                    //       final quizzes = snapshot.data ?? [];
-                    //       quizProvider.setquizzes(quizzes);
-
-                    //       // return quizzes.isNotEmpty
-                    //       //     ? ListView.builder(
-                    //       //         itemCount: quizzes.length,
-                    //       //         itemBuilder:
-                    //       //             (BuildContext context, int index) {
-                    //       //           return ListTile(
-                    //       //               title: Text("List item $index"));
-                    //       //         })
-                    //       //     : const Text('No quizzes yet.');
-                    //       return ListView.builder(
-                    //           itemCount: quizzes.length,
-                    //           itemBuilder: (BuildContext context, int index) {
-                    //             return ListTile(
-                    //                 title: Text("List item ${quizzes[0]}"));
-                    //           });
-                    //     })
-                  ],
-                ),
-              )
-            ],
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
           ),
-        );
-      },
+        ],
+      ),
     );
   }
 }
